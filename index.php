a9d1c9b9fd9e507bbb560d0fda89e8df3595ebff
Severin Mbekou <sgmbekou@gmail.com>
2018-05-24 14:15:21 +0100

    Update demo Access just download and try

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require("services/CustumApi.php");
 
 
 // Récupération d'une instance de la classe principale du sdk: API + configuration des donnés du developpeur.
-$custumApi = CustumAPI::getInstance("Client_id", "client_token", "username", "password");
+$custumApi = CustumAPI::getInstance("25_1m12uorsvdtwocow80ocs04sg4ks40wwc8goc40skswk4ooggg", "cuf1w16nnhckgkcccc00gkko40sg08socwwo8kg40oog40gk0", "demoapi", "demoapi");
 
 // Autentification du développeur et récupération du token d'accées ou mise à jour de ce dernier.
 $acessTk = $custumApi->oauthAuthenticate();
@@ -15,8 +15,8 @@ echo "\n \n \n The access token is: $acessTk \n";
 echo "\n \n \n";
 
 // Here we will checkout customer with phoneNumber XXXXXXXXX with amount YYYYYYYY
-$phoneNumber = "670992438"; // Numéro du client à débiter should be 9 digits.
-$amount = "75";              // Montant à débiter.
+$phoneNumber = "677925286"; // Numéro du client à débiter should be 9 digits.
+$amount = "100";              // Montant à débiter.
 
 // Demande de paiement adressée au client pour validation sur son téléphone portable.
 $resPayment = $custumApi->requestPayment($amount, $phoneNumber);
